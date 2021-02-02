2722aab747378a3f181062bd995ff008e400a0f1
Giulio <giulio.tavoni@gmail.com>
2021-02-02 17:16:20 +0100

    Aggiornamento contatti e messaggi tramite vue

--- js/script.js
+++ js/script.js
@@ -13,16 +13,19 @@ var app = new Vue({
           {
             text: 'Quando ci vediamo?',
             date: '02.02.2021 ore 10.15',
+            shortdate: '10.15',
             type: 'received'
           },
           {
             text: 'Domani va bene?',
             date: '02.02.2021 ore 10.18',
+            shortdate: '10.18',
             type: 'sent'
           },
           {
             text: 'Ok, a domani',
             date: '02.02.2021 ore 10.18',
+            shortdate: '10.18',
             type: 'received'
           }
         ]
@@ -36,21 +39,25 @@ var app = new Vue({
           {
             text: 'A che ora giochi domani?',
             date: '02.02.2021 ore 10.40',
+            shortdate: '10.40',
             type: 'sent'
           },
           {
             text: 'Alle 15.30',
             date: '02.02.2021 ore 10.40',
+            shortdate: '10.40',
             type: 'received'
           },
           {
             text: 'Ok, ti vengo a vedere',
             date: '02.02.2021 ore 10.41',
+            shortdate: '10.41',
             type: 'sent'
           },
           {
             text: 'Grande, ti aspetto!',
             date: '02.02.2021 ore 10.41',
+            shortdate: '10.41',
             type: 'received'
           }
         ]
@@ -64,16 +71,19 @@ var app = new Vue({
           {
             text: 'Vado al cinema',
             date: '02.02.2021 ore 12.24',
+            shortdate: '12.24',
             type: 'sent'
           },
           {
             text: 'Che film danno?',
             date: '02.02.2021 ore 12.25',
+            shortdate: '12.25',
             type: 'received'
           },
           {
             text: 'Tenet',
             date: '02.02.2021 ore 12.26',
+            shortdate: '12.26',
             type: 'sent'
           }
         ]
@@ -87,16 +97,19 @@ var app = new Vue({
           {
             text: 'Domani vado a Roma a trovare Paolo',
             date: '02.02.2021 ore 12.45',
+            shortdate: '12.45',
             type: 'received'
           },
           {
             text: 'Ah salutamelo allora!',
             date: '02.02.2021 ore 12.46',
+            shortdate: '12.46',
             type: 'sent'
           },
           {
             text: 'Certo!',
             date: '02.02.2021 ore 12.46',
+            shortdate: '12.46',
             type: 'received'
           },
         ]
@@ -110,16 +123,19 @@ var app = new Vue({
           {
             text: 'asdhvbkaslhdvbasklhdvbldsfhvbajskdvbasjdvbhasjkdvbhajskdvb',
             date: '02.02.2021 ore 12.45',
+            shortdate: '12.45',
             type: 'received'
           },
           {
             text: 'Eh?',
             date: '02.02.2021 ore 12.46',
+            shortdate: '12.46',
             type: 'sent'
           },
           {
-            text: 'Ahahah scusa mi è partito un messaggio',
-            date: '02.02.2021 ore 12.46',
+            text: 'Ah scusa mi è partito un messaggio',
+            date: '12.46',
+            shortdate: '12.46',
             type: 'received'
           },
         ]
@@ -133,20 +149,24 @@ var app = new Vue({
           {
             text: 'Dove sei?',
             date: '02.02.2021 ore 12.52',
+            shortdate: '12.52',
             type: 'received'
           },
           {
             text: 'Sono in riunione',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'sent'
           },
           {
             text: 'Ok quando hai finito chiamami',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'received'
           }
         ]
       },
+      // oggetto 7
       {
         name: 'Mario',
         avatar: '_7',
@@ -155,20 +175,24 @@ var app = new Vue({
           {
             text: 'Dove sei?',
             date: '02.02.2021 ore 12.52',
+            shortdate: '12.52',
             type: 'received'
           },
           {
             text: 'Sono in riunione',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'sent'
           },
           {
             text: 'Ok quando hai finito chiamami',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'received'
           }
         ]
       },
+      // oggetto 8
       {
         name: 'Giorgio',
         avatar: '_8',
@@ -177,22 +201,31 @@ var app = new Vue({
           {
             text: 'Dove sei?',
             date: '02.02.2021 ore 12.52',
+            shortdate: '12.52',
             type: 'received'
           },
           {
             text: 'Sono in riunione',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'sent'
           },
           {
             text: 'Ok quando hai finito chiamami',
             date: '02.02.2021 ore 12.53',
+            shortdate: '12.53',
             type: 'received'
           }
         ]
       }
+    ], // fine contatti
+    utente: {
+      nome: 'Giulio',
+      cognome: 'Tavoni',
+      access: '02.02.2021 ore 15.47',
+      description: 'Playing on'
+    }
 
-    ]
-  }
+  } // fine data
 
-})
+});
